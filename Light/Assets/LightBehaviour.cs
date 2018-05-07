f144b228b49d1b766d1806b6d76925ebf5c3e3d4
Matthew <matthewgoodall1579@gmail.com>
2018-05-07 13:37:55 -0700

    Light Started

---
+++ Light/Assets/LightBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBehaviour : MonoBehaviour {
+
+    private Light lightSource;
+    private Color color;
+    private float timePassed;
+    private float changeValue;
+
+	// Use this for initialization
+	void Start () {
+        lightSource = this.GetComponent<Light>();
+        if (lightSource != null) {
+            color = lightSource.color;
+        }
+        changeValue = 0;
+        timePassed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timePassed = Time.time;
+        timePassed = timePassed - Mathf.Floor(timePassed);
+
+        lightSource.color = color * CalculateChange();
+	}
+
+    private float CalculateChange()
+    {
+        changeValue = -Mathf.Sin(timePassed * 12 * Mathf.PI) * 0.05f + 0.95f;
+        return changeValue;
+    }
+}
---
+++ Light/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private float speed = 20f;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (vertical != 0){
+            this.transform.position += new Vector3(0, 0, vertical * speed * Time.deltaTime);
+        }
+        if (horizontal != 0) {
+            this.transform.position += new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
+        }
+
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        }
+    }
+
+}
