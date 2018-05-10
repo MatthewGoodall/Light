cdfb8207eb2af0085bd48ef04b85073793ce684b
Matthew <matthewgoodall1579@gmail.com>
2018-05-10 11:51:19 -0700

    Start Game Started

--- Light/Assets/Scripts/GameManager.cs
+++ Light/Assets/Scripts/GameManager.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public GameObject ground;
     public GameObject rune;
+
+    public TextMeshProUGUI timer;
+    public Button startGame;
+
+    private float currentTime = 30;
+    private float beginGame = 3;
+
+    public static bool gameStarted = false;
 
     public int amountOfRunes;
     public int amountOfEnemies;
@@ -13,17 +23,51 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        for (int i = 0; i < amountOfRunes; i++) {
+        SpawnRunes();
+        //SpawnEnemies();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameStarted)
+        {
+            timer.text = Mathf.FloorToInt(currentTime).ToString();
+            currentTime -= Time.deltaTime;
+        }
+        else {
+            
+            if (beginGame > 0)
+            {
+                timer.text = Mathf.FloorToInt(beginGame).ToString();
+                beginGame -= Time.deltaTime;
+            }
+            else
+            {
+                gameStarted = true;
+            }
+        }
+
+    }
+
+    private void SpawnRunes()
+    {
+        for(int i = 0; i < amountOfRunes; i++) {
             float x = Random.Range(-60, 60);
             float z = Random.Range(-60, 60);
             Vector3 runePosition = new Vector3(x, 1.5f, z);
             GameObject runeInstantiated = Instantiate<GameObject>(rune, runePosition, rune.transform.rotation);
             runeInstantiated.gameObject.transform.SetParent(ground.transform);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    private void SpawnEnemies() {
+        for(int i = 0; i < amountOfEnemies; i++) {
+            float x = Random.Range(-60, 60);
+            float z = Random.Range(-60, 60);
+            Vector3 enemyPosition = new Vector3(x, 1.5f, z);
+            GameObject enemyInstantiated = Instantiate<GameObject>(rune, enemyPosition, rune.transform.rotation);
+            enemyInstantiated.gameObject.transform.SetParent(ground.transform);
+        }
+    }
 }
--- Light/Assets/Scripts/PlayerMotor.cs
+++ Light/Assets/Scripts/PlayerMotor.cs
@@ -18,27 +18,32 @@ public class PlayerMotor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        if (vertical != 0){
-            this.transform.position += new Vector3(0, 0, vertical * speed * Time.deltaTime);
-        }
-        if (horizontal != 0) {
-            this.transform.position += new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
-        }
+        if (GameManager.gameStarted) {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            if (vertical != 0)
+            {
+                this.transform.position += new Vector3(0, 0, vertical * speed * Time.deltaTime);
+            }
+            if (horizontal != 0)
+            {
+                this.transform.position += new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
+            }
 
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float hitdist = 0.0f;
-        if (playerPlane.Raycast(ray, out hitdist))
-        {
-            Vector3 targetPoint = ray.GetPoint(hitdist);
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-        }
+            Plane playerPlane = new Plane(Vector3.up, transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitdist = 0.0f;
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+            }
 
-        if (Input.GetMouseButtonDown(0)) {
-            currentRune.Fire();
+            if (Input.GetMouseButtonDown(0))
+            {
+                currentRune.Fire();
+            }
         }
     }
 
